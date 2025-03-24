947680596644f214f704e2e9a39d8793e3aa30ca
Nazrul Islam <nazrulislam@Nazruls-MacBook.local>
2025-03-24 16:35:25 +0600

    userHashpass

--- model/user.model.js
+++ model/user.model.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const db = require('../config/db');
+const bcrypt = require('bcryptjs');
 
 const { Schema }= mongoose;
 
@@ -14,6 +15,19 @@ const userSchema =  new Schema({
         type:String,
         required:true
     }
+});
+
+userSchema.pre('save', async function() {
+    try{
+        var user = this;
+        const salt = await(bcrypt.genSalt(12));
+        const hashPass = await bcrypt.hash(user.password,salt);
+        user.password = hashPass;
+
+    }catch(err){
+        throw err;
+    }
+    
 });
 
 const userModel = db.model('user',userSchema);
